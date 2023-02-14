9abee9cae39c6245b0fb72318e6108f2e4c757d3
LeonardoContino <leoconti2000@hotmail.it>
2023-02-14 16:27:24 +0100

    ex 2 group by

--- db-ex/tracce SELECT & GROUP BY.sql
+++ db-ex/tracce SELECT & GROUP BY.sql
@@ -51,7 +51,9 @@ SELECT COUNT(*) AS 'iscritti', YEAR(enrolment_date)
 FROM students
 GROUP BY YEAR(enrolment_date);
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-
+SELECT COUNT(*) AS 'insegnanti', (office_address) 
+FROM (teachers)
+GROUP BY(office_address);
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 
