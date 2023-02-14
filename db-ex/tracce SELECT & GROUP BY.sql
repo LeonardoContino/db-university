ad65429ebe5c10a5a4ecb72f080cfbdfe9427720
LeonardoContino <leoconti2000@hotmail.it>
2023-02-14 16:43:44 +0100

    ex 3

--- db-ex/tracce SELECT & GROUP BY.sql
+++ db-ex/tracce SELECT & GROUP BY.sql
@@ -56,6 +56,8 @@ FROM (teachers)
 GROUP BY(office_address);
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-
+SELECT (exam_id) AS 'appello', AVG(vote) AS 'media voto'
+FROM (exam_student)
+GROUP BY (exam_id);
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
