d81f0614ddc02be230a972e430462995cc73766e
LeonardoContino <leoconti2000@hotmail.it>
2023-02-15 15:46:26 +0100

    ex 5

--- db-ex/traccia JOIN.sql
+++ db-ex/traccia JOIN.sql
@@ -32,5 +32,13 @@ JOIN `departments`
 ON `departments`.`id`= `degrees`.`department_id`
 ORDER BY `students`.`surname`;
 --5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+SELECT `degrees`.`name`, `courses`.`name`, `teachers`.`name`,`teachers`.`surname` 
+FROM `degrees`
+JOIN `courses`
+ON `courses`.`degree_id`= `degrees`.`id`
+JOIN `course_teacher`
+ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`;
 --6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 --7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per  superare ciascuno dei suoi esami
