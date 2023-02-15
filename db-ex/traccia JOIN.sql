ca4cb3e783261772cc16f00197143bc2933637a9
LeonardoContino <leoconti2000@hotmail.it>
2023-02-15 15:18:55 +0100

    ex 1

--- db-ex/traccia JOIN.sql
+++ db-ex/traccia JOIN.sql
@@ -1,5 +1,9 @@
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
-
+SELECT `students`.`name`, `students`.`surname`, `degrees`.`name`
+FROM `students`
+JOIN `degrees`
+ON `degrees`.`id` = `students`.`degree_id`
+WHERE `degrees`.`name` = 'corso di laurea in economia';
 --2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 --3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 --4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
