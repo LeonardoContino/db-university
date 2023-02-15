ffab328be3f555aed96d19d15e56885528c1330e
LeonardoContino <leoconti2000@hotmail.it>
2023-02-15 09:41:32 +0100

    mod ex 3

--- db-ex/tracce SELECT & GROUP BY.sql
+++ db-ex/tracce SELECT & GROUP BY.sql
@@ -13,7 +13,7 @@ WHERE (cfu) > 10;
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 SELECT (name), (surname), (date_of_birth)
 FROM (students)
-WHERE YEAR(date_of_birth) < 1992;
+WHERE (YEAR(CURDATE()) - YEAR(date_of_birth)) >= 30;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT (id),(period),(year)
