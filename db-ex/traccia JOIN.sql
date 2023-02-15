a8a39f59b9b3b6a8574ce243a1f4afaf48b2403a
LeonardoContino <leoconti2000@hotmail.it>
2023-02-15 15:33:06 +0100

    ex 3

--- db-ex/traccia JOIN.sql
+++ db-ex/traccia JOIN.sql
@@ -14,6 +14,14 @@ ON `departments`.id = `degrees`.`department_id`
 WHERE `departments`.`name` = 'dipartimento di neuroscienze';
 
 --3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+SELECT `courses`.`name`, `teachers`.`id`,`teachers`.`name`, `teachers`.`surname`
+FROM `courses`
+JOIN `course_teacher`
+ON `course_teacher`.`course_id` = `courses`.`id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+WHERE `teachers`.`id` = 44;
 --4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 --relativo dipartimento, in ordine alfabetico per cognome e nome
 --5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
